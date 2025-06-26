399392a6a8fae33594dec9218864fe55627fa6af
Won Jiyeon <singlerose96@naver.com>
2025-06-26 22:34:55 +0900

    도전과제디벨롭

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -18,7 +18,8 @@ void main(List<String> arguments) {
     print('2: 장바구니에 상품 담기');
     print('3: 장바구니 보기');
     print('4: 종료하기');
-    stdout.write('입력: ');
+    print('6: 장바구니 초기화');
+    stdout.write('\n무엇을 도와드릴까요?: ');
     String? input = stdin.readLineSync();
 
     switch (input) {
@@ -28,14 +29,14 @@ void main(List<String> arguments) {
         break;
 
       case '2':
-        stdout.write('\n담을 상품 이름: ');
+        stdout.write('\n상품 이름을 입력하세요 : ');
         String? name = stdin.readLineSync();
         if (name == null || !products.containsKey(name)) {
           print('입력값이 올바르지 않아요 !');
           break;
         }
 
-        stdout.write('담을 개수: ');
+        stdout.write('상품 개수를 입력하세요 : ');
         int? qty = int.tryParse(stdin.readLineSync() ?? '');
         if (qty == null) {
           print('입력값이 올바르지 않아요 !');
@@ -54,20 +55,59 @@ void main(List<String> arguments) {
 
       case '3':
         if (cart.isEmpty) {
-          print('\n🛒 장바구니가 비어 있어요.');
+          print('\n!!!🛒 장바구니가 비어 있어요!!!');
         } else {
-          print('\n🛒 장바구니 내용:');
-          cart.forEach((name, qty) => print('$name x $qty'));
+          print('\n🩶 🩶 🩶 장바구니 내용 🩶 🩶 🩶 :');
+          cart.forEach((name, qty) => print('- $name x $qty'));
+
+         int total = 0;
+         cart.forEach((name, qty) {//장바구니의 가격을 int타입으로 변환
+          String? priceString = products[name]!.replaceAll('원', '');
+          int price = int.parse(priceString.replaceAll('원',''));
+          int subtotal = price * qty;
+          print('- $name x $qty개 = $subtotal원'); //담긴목록
+
+          total += subtotal;
+         });
+         print('\n- 총 $total 원'); //총가격 출력
+
+         int giftGoal = 20000; //사은품조건
+
+      if(total >= giftGoal) {
+        print('\n❤️ $giftGoal 원 이상 구매하셔서 사은품으로 비누를 드려요 ❤️');
+         cart['비누'] = 1;
+      } else {
+        print('❤️ $giftGoal 원 구매하시면 사은품으로 비누를 드려요! ❤️');
+      }
+      }
+        break;
+
+       case '4':
+        print('\n진짜 가실려구요....?');
+        stdout.write('\n5 를 눌러서 나가기 // ');
+        print('  이어서 쇼핑하려면 아무키나 누르세요');
+
+        String? exit = stdin.readLineSync();
+
+        if(exit == '5') {
+          print('\n다음에 또 만나요!');
+          isRunning = false;
         }
+          else {
+            print('휴! 정말 가시는 줄 알았어요!');
+          }
         break;
 
-      case '4':
-        print('\n이용해 주셔서 감사합니다 ~ 안녕히 가세요 !');
-        isRunning = false;
-        break;
-
-      default:
-        print('❗️잘못된 입력입니다.');
+      case '6':
+       if (cart.isEmpty) {
+       print('\n이미 장바구니가 비어있어요.');
+       } else {
+       cart.clear(); // 장바구니 초기화
+        print('\n장바구니가 비워졌어요.');
+       }
+      break;
+  
+      default : print('❗️잘못된 입력입니다.');
     }
   }
 }
